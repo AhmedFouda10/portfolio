4fe1c3af2e022e4e39642d51ccf514443fb0d756
Ahmed Fouda <58745303+AhmedFouda10@users.noreply.github.com>
2022-09-13 03:14:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,8 +56,8 @@ let calcScrollValue = () => {
     scrollProgress.addEventListener("click",() => {
         document.documentElement.scrollTop = 0;
     });
-    scrollProgress.style.background= `conic-gradient 
-    (#03cc65 ${scrollValue}% , #d7d7d7 ${scrollValue}%)`;
+//     scrollProgress.style.background= `conic-gradient 
+//     (#03cc65 ${scrollValue}% , #d7d7d7 ${scrollValue}%)`;
 };
 window.onscroll=calcScrollValue;
 window.onload=calcScrollValue;
